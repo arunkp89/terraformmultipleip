a93a082ecebab54898a027c6671acd841dd592f2
Arun Kumar <arukum@microsoft.com>
2020-03-20 01:23:32 -0500

    fv

--- sample2.tf
+++ sample2.tf
@@ -1,34 +1,20 @@
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
-    # The "feature" block is required for AzureRM provider 2.x. 
-    # If you're using version 1.x, the "features" block is not allowed.
     version = "~>2.0"
     features {}
 }
 
-# Create a resource group if it doesn't exist
 resource "azurerm_resource_group" "myterraformgroup" {
-    name     = "myResourceGroup"
+    name     = "salesforce"
     location = "eastus"
-
-    tags = {
-        environment = "Terraform Demo"
-    }
 }
 
-# Create virtual network
 resource "azurerm_virtual_network" "myterraformnetwork" {
     name                = "myVnet"
     address_space       = ["10.0.0.0/16"]
     location            = "eastus"
     resource_group_name = azurerm_resource_group.myterraformgroup.name
-
-    tags = {
-        environment = "Terraform Demo"
-    }
 }
 
-# Create subnet
 resource "azurerm_subnet" "myterraformsubnet" {
     name                 = "mySubnet"
     resource_group_name  = azurerm_resource_group.myterraformgroup.name
@@ -36,19 +22,12 @@ resource "azurerm_subnet" "myterraformsubnet" {
     address_prefix       = "10.0.1.0/24"
 }
 
-# Create public IPs
 resource "azurerm_public_ip" "myterraformpublicip" {
     name                         = "myPublicIP"
     location                     = "eastus"
     resource_group_name          = azurerm_resource_group.myterraformgroup.name
     allocation_method            = "Dynamic"
-
-    tags = {
-        environment = "Terraform Demo"
-    }
 }
-
-# Create Network Security Group and rule
 resource "azurerm_network_security_group" "myterraformnsg" {
     name                = "myNetworkSecurityGroup"
     location            = "eastus"
@@ -65,13 +44,8 @@ resource "azurerm_network_security_group" "myterraformnsg" {
         source_address_prefix      = "*"
         destination_address_prefix = "*"
     }
-
-    tags = {
-        environment = "Terraform Demo"
-    }
 }
 
-# Create network interface
 resource "azurerm_network_interface" "myterraformnic" {
     name                      = "myNIC"
     location                  = "eastus"
@@ -140,42 +114,14 @@ resource "azurerm_network_interface" "myterraformnic" {
         private_ip_address_allocation = "Static"
         private_ip_address            = "10.0.1.20"
     }
-    tags = {
-        environment = "Terraform Demo"
-    }
 }
 
-# Connect the security group to the network interface
 resource "azurerm_network_interface_security_group_association" "example" {
     network_interface_id      = azurerm_network_interface.myterraformnic.id
     network_security_group_id = azurerm_network_security_group.myterraformnsg.id
 }
 
-# Generate random text for a unique storage account name
-resource "random_id" "randomId" {
-    keepers = {
-        # Generate a new ID only when a new resource group is defined
-        resource_group = azurerm_resource_group.myterraformgroup.name
-    }
-    
-    byte_length = 8
-}
-
-# Create storage account for boot diagnostics
-resource "azurerm_storage_account" "mystorageaccount" {
-    name                        = "diag${random_id.randomId.hex}"
-    resource_group_name         = azurerm_resource_group.myterraformgroup.name
-    location                    = "eastus"
-    account_tier                = "Standard"
-    account_replication_type    = "LRS"
-
-    tags = {
-        environment = "Terraform Demo"
-    }
-}
-
-# Create virtual machine
-resource "azurerm_linux_virtual_machine" "myterraformvm" {
+resource "azurerm_linux_virtual_machine" "terraformvm" {
     name                  = "myVM"
     location              = "eastus"
     resource_group_name   = azurerm_resource_group.myterraformgroup.name
@@ -196,15 +142,8 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
     }
 
     computer_name  = "myvm"
-    admin_username = "azureuser"
+    admin_username = "azure"
     admin_password = "Password1234!"
     disable_password_authentication = false
-        
-    boot_diagnostics {
-        storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
-    }
-
-    tags = {
-        environment = "Terraform Demo"
-    }
+    
 }
